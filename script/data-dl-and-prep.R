1fdc562de045064468a4b63ffdf0007f7987b43c
Mathieu Lehot <mathieu.lehot@francetv.fr>
2025-04-09 14:53:28 +0200

    fix:script/data-dl-and-prep.R

--- script/data-dl-and-prep.R
+++ script/data-dl-and-prep.R
@@ -1,6 +1,6 @@
-library(tidyverse)
-library(lubridate)
-library(tidyquant)
+#PACKAGE USED : magrittr, dplyr, tidyr, lubridate, tidyquant, readr
+
+library(magrittr)
 
 # Liste des valeurs boursières à télécharger
 
@@ -17,13 +17,13 @@ list_of_markets <- c("^DJI", # Dow Jones Industrial Average
 
 markets_data <- purrr::map_dfr(list_of_markets, function(x) {
   
-  tq_get(x, from="2025-01-01", to=Sys.Date())
+  tidyquant::tq_get(x, from="2025-01-01", to=Sys.Date())
   
 })
 
 # Tri des données de marché
 tidy_markets_data <- markets_data %>%
-  mutate(market=case_when(
+  dplyr::mutate(market = dplyr::case_when(
     symbol == "^DJI" ~ "New York",
     symbol == "^GDAXI" ~ "Francfort", # Dax
     symbol == "^FCHI" ~ "Paris", # CAC 40
@@ -32,39 +32,39 @@ tidy_markets_data <- markets_data %>%
     symbol == "000001.SS" ~ "Shanghaï", # SSE COMPOSITE INDEX
     symbol == "^HSI"  ~ "Hong Kong" # HANG SENG
   )) %>%
-  select(-symbol)
+  dplyr::select(-symbol)
 
 
 # Valeurs de référence (pour la base 100) (les valeurs des marchés à leur clôture, le vendredi 17 janvier)
 base_value_df <- tidy_markets_data %>%
-  filter(date==ymd("2025-01-17")) %>%
-  select(market, base_value=close)
+  dplyr::filter(date == lubridate::ymd("2025-01-17")) %>%
+  dplyr::select(market, base_value=close)
 
 # Transposition des valeurs journalières en base 100
 markets_data_into_base100 <- tidy_markets_data %>%
-  select(date, close, market) %>%
-  filter(date>=ymd("2025-01-17")) %>%
-  left_join(base_value_df, by = "market") %>%
-  mutate(evol_base_100 = (close/base_value)*100-100) %>%
-  select(date, evol_base_100, market)
+  dplyr::select(date, close, market) %>%
+  dplyr::filter(date>=lubridate::ymd("2025-01-17")) %>%
+  dplyr::left_join(base_value_df, by = "market") %>%
+  dplyr::mutate(evol_base_100 = (close/base_value)*100-100) %>%
+  dplyr::select(date, evol_base_100, market)
 
 # Transposition des données pour Flourish
-tidy_data_to_flourish <- data.frame(date=seq(ymd("2025-01-17"), ymd("2025-04-07"), by = "1 day")) %>%
-  left_join(markets_data_into_base100, by = "date") %>%
-  mutate(market=ifelse(is.na(market), "remove", market)) %>%
-  spread(key="market", value="evol_base_100", fill="") %>%
-  select(-remove) %>%
-  mutate(mois=as.character(month(date, label=T, abbr=F))) %>%
-  mutate(date_axis = ifelse(day(date)==1, mois, as.character(date)),
-         date_pop_up = ifelse(day(date)==1,
-                              paste0(day(date), "er ", month(date, label=T, abbr=F), " ", year(date)),
-                              paste0(day(date), " ", month(date, label=T, abbr=F), " ", year(date))
+tidy_data_to_flourish <- data.frame(date=seq(lubridate::ymd("2025-01-17"), lubridate::ymd("2025-04-07"), by = "1 day")) %>%
+  dplyr::left_join(markets_data_into_base100, by = "date") %>%
+  dplyr::mutate(market=ifelse(is.na(market), "remove", market)) %>%
+  tidyr::spread(key="market", value="evol_base_100", fill="") %>%
+  dplyr::select(-remove) %>%
+  dplyr::mutate(mois=as.character(lubridate::month(date, label=T, abbr=F))) %>%
+  dplyr::mutate(date_axis = ifelse(lubridate::day(date)==1, mois, as.character(date)),
+         date_pop_up = ifelse(lubridate::day(date)==1,
+                              paste0(lubridate::day(date), "er ", lubridate::month(date, label=T, abbr=F), " ", lubridate::year(date)),
+                              paste0(lubridate::day(date), " ", lubridate::month(date, label=T, abbr=F), " ", lubridate::year(date))
          )) %>%
-  select(-mois)
+  dplyr::select(-mois)
 
 
 #Ecriture des données
-write_csv(tidy_data_to_flourish, "data/tidy_markets_data_to_flourish.csv")
+readr::write_csv(tidy_data_to_flourish, "data/tidy_markets_data_to_flourish.csv")
 
 
 
